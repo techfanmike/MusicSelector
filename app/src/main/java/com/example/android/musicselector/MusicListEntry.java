0a7667fd91ce36c7852b9e576045310f18df87c9
mike <redmike@mindspring.com>
2019-03-04 01:25:10 -0500

    Comments at accessors

--- app/src/main/java/com/example/android/musicselector/MusicListEntry.java
+++ app/src/main/java/com/example/android/musicselector/MusicListEntry.java
@@ -1,13 +1,33 @@
 package com.example.android.musicselector;
 
+// A data class to contain the information for each music list item
 public class MusicListEntry {
-    public String mArtist;
-    public String mAlbum;
-    public String mSong;
+    private String mArtist;
+    private String mAlbum;
+    private String mSong;
 
+    // assign the member variables in the constructor
     public MusicListEntry(String Artist, String Album, String Song){
         mArtist = Artist;
         mAlbum = Album;
         mSong = Song;
+    }
+
+    // define the accessor for artist
+    public String getArtist()
+    {
+        return mArtist;
+    }
+
+    // define the accessor for album
+    public String getAlbum()
+    {
+        return mAlbum;
+    }
+
+    // define the accessor for song
+    public String getSong()
+    {
+        return mSong;
     }
 }
--- app/src/main/java/com/example/android/musicselector/MusicListEntryAdapter.java
+++ app/src/main/java/com/example/android/musicselector/MusicListEntryAdapter.java
@@ -9,8 +9,6 @@ import android.widget.TextView;
 
 import java.util.ArrayList;
 
-import static android.support.v4.content.ContextCompat.getSystemService;
-
 public class MusicListEntryAdapter extends ArrayAdapter<MusicListEntry> {
     public MusicListEntryAdapter(Context context, int resource, ArrayList<MusicListEntry> MusicList) {
         super(context, resource, MusicList);
@@ -20,18 +18,21 @@ public class MusicListEntryAdapter extends ArrayAdapter<MusicListEntry> {
     public View getView(int position, View convertView, ViewGroup container) {
         MusicListEntry entry = getItem(position);
 
-        if(convertView == null) {
+        // if a new list, inflate a new list item
+        if (convertView == null) {
             LayoutInflater inflater = LayoutInflater.from(getContext());
             convertView = inflater.inflate(R.layout.music_list_item_layout, container, false);
-            }
+        }
 
+        // get the text fields from the layout file
         TextView artist = convertView.findViewById(R.id.artistName);
         TextView album = convertView.findViewById(R.id.albumName);
         TextView song = convertView.findViewById(R.id.song);
 
-        artist.setText(entry.mArtist);
-        album.setText(entry.mAlbum);
-        song.setText(entry.mSong);
+        // set the text fields for the list item
+        artist.setText(entry.getArtist());
+        album.setText(entry.getAlbum());
+        song.setText(entry.getSong());
 
         return convertView;
     }
