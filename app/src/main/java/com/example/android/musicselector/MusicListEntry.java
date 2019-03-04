68012b679fabe906e0fe7bba35e2f46ac3f5be52
mike <redmike@mindspring.com>
2019-03-04 01:44:35 -0500

    Cleanup

--- app/src/main/java/com/example/android/musicselector/MusicListEntry.java
+++ app/src/main/java/com/example/android/musicselector/MusicListEntry.java
@@ -2,32 +2,29 @@ package com.example.android.musicselector;
 
 // A data class to contain the information for each music list item
 public class MusicListEntry {
-    private String mArtist;
-    private String mAlbum;
-    private String mSong;
+    private String mArtist = "";
+    private String mAlbum = "";
+    private String mSong = "";
 
     // assign the member variables in the constructor
-    public MusicListEntry(String Artist, String Album, String Song){
+    public MusicListEntry(String Artist, String Album, String Song) {
         mArtist = Artist;
         mAlbum = Album;
         mSong = Song;
     }
 
     // define the accessor for artist
-    public String getArtist()
-    {
+    public String getArtist() {
         return mArtist;
     }
 
     // define the accessor for album
-    public String getAlbum()
-    {
+    public String getAlbum() {
         return mAlbum;
     }
 
     // define the accessor for song
-    public String getSong()
-    {
+    public String getSong() {
         return mSong;
     }
 }
