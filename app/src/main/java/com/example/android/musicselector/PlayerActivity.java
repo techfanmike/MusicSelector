620b6b808e3aea42f3788956c0f71f6a26e4a1c4
mike <redmike@mindspring.com>
2019-03-08 19:28:46 -0500

    Add activity

---
+++ app/src/main/java/com/example/android/musicselector/PlayerActivity.java
@@ -0,0 +1,13 @@
+package com.example.android.musicselector;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PlayerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_player);
+    }
+}
